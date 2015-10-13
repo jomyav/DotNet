42387d38ed62e9c8daf261619e45adfa0ab69d6e
jomyav <jomyav007@gmail.com>
2015-10-13 17:17:29 +0530

    Testing

---
+++ Polymorphism Testing/Models.cs
@@ -0,0 +1,71 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace InheritanceTesting
+{
+    public class A
+    {
+        public virtual void show()
+        {
+            Console.WriteLine("Hello: From A");
+            Console.ReadLine();
+        }
+    }
+    public class B : A
+    {
+        public override void show()
+        {
+            Console.WriteLine("Hello: From B");
+            Console.ReadLine();
+        }
+    }
+
+    public class C : A
+    {
+        public void show()
+        {
+            Console.WriteLine("Hello: From C");
+            Console.ReadLine();
+        }
+    }
+
+    public class D
+    {
+        public void show()
+        {
+            Console.WriteLine("Hello: From D");
+            Console.ReadLine();
+        }
+    }
+
+    public class E : D
+    {
+        public new void show()
+        {
+            Console.WriteLine("Hello: From E");
+            Console.ReadLine();
+        }
+    }
+
+    public class F : D
+    {
+        public void show()
+        {
+            Console.WriteLine("Hello: From F");
+            Console.ReadLine();
+        }
+    }
+
+    public class G : A
+    {
+        public new void show()
+        {
+            Console.WriteLine("Hello: From G");
+            Console.ReadLine();
+        }
+    }
+
+}
---
+++ Polymorphism Testing/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace InheritanceTesting
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //Hello: From B
+            A a1 = new B();
+            a1.show();
+
+            //Hello: From A
+            A a2 = new C();
+            a2.show();
+
+            //Hello: From D
+            D d1 = new E();
+            d1.show();
+
+            //Hello: From D
+            D d2 = new F();
+            d2.show();
+
+            //Hello: From A
+            A a3 = new G();
+            a2.show();
+
+        }
+    }
+}
